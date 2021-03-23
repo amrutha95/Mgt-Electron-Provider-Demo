9d49f36803eb08e7898ffd46ef5127abac659fbd
Amrutha Srinivasan <amsrin@microsoft.com>
2021-03-23 13:39:44 -0700

    Removed cache plugin

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,6 @@ import {
   ElectronAuthenticator,
   MsalElectronConfig,
 } from '@microsoft/mgt-electron-provider/dist/Authenticator';
-import {
-  FilePersistenceWithDataProtection,
-  DataProtectionScope,
-  KeychainPersistence,
-  LibSecretPersistence,
-  PersistenceCachePlugin,
-} from '@azure/msal-node-extensions';
 
 async function createWindow() {
   // Create the browser window.
@@ -24,8 +17,6 @@ async function createWindow() {
     icon: path.join(__dirname, '../assets/icons/clipboard.png'),
     width: 800,
   });
-  const cachePath = './data/cache.json'; //Change this if needed
-  const filePersistence = await createPersistence(cachePath);
   const config: MsalElectronConfig = {
     clientId: 'e5774a5b-d84d-4f30-892e-c4f73a503943',
     mainWindow: mainWindow, //This is the BrowserWindow instance that requires authentication
@@ -42,7 +33,6 @@ async function createWindow() {
       'Sites.Read.All',
       'Sites.ReadWrite.All',
     ],
-    cachePlugin: new PersistenceCachePlugin(filePersistence),
   };
   ElectronAuthenticator.initialize(config);
 
@@ -82,28 +72,6 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
-async function createPersistence(cachePath: string) {
-  // On Windows, uses a DPAPI encrypted file
-  if (process.platform === 'win32') {
-    return FilePersistenceWithDataProtection.create(
-      cachePath,
-      DataProtectionScope.CurrentUser
-    );
-  }
-
-  // On Mac, uses keychain.
-  if (process.platform === 'darwin') {
-    return KeychainPersistence.create(cachePath, 'serviceName', 'accountName'); // Replace serviceName and accountName
-  }
-
-  // On Linux, uses  libsecret to store to secret service. Libsecret has to be installed.
-  if (process.platform === 'linux') {
-    return LibSecretPersistence.create(cachePath, 'serviceName', 'accountName'); // Replace serviceName and accountName
-  }
-
-  throw new Error('Could not create persistence. Platform not supported');
-}
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
